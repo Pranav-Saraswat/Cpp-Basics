a5c09fbba41caba54bdb1a8047a683431ae2bb1c
Pranav Saraswat <pranavsaraswat20040124@gmail.com>
2023-01-08 01:53:49 +0530

    add some files

--- cpp/arithmatic operators.cpp
+++ cpp/arithmatic operators.cpp
@@ -10,3 +10,89 @@ int main() {
   cout << sum3;
   return 0;
 }
+
+
+
+// Addition
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  int y = 3;
+  cout << x + y;
+  return 0;
+}
+
+// Subtraction
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  int y = 3;
+  cout << x - y;
+  return 0;
+}
+
+//Multiplication
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  int y = 3;
+  cout << x * y;
+  return 0;
+}
+
+// Division
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 12;
+  int y = 3;
+  cout << x / y;
+  return 0;
+}
+
+// Modulus
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  int y = 2;
+  cout << x % y;
+  return 0;
+}
+
+// Increment
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  ++x;
+  cout << x;
+  return 0;
+}
+
+// Decrement
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  --x;
+  cout << x;
+  return 0;
+}
--- cpp/assignment operators.cpp
+++ cpp/assignment operators.cpp
@@ -7,3 +7,144 @@ int main() {
   cout << x;
   return 0;
 }
+
+
+// = assign
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  cout << x;
+  return 0;
+}
+
+
+// += assign
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  x += 3;
+  cout << x;
+  return 0;
+}
+
+
+// -= assign
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  x -= 3;
+  cout << x;
+  return 0;
+}
+
+// *= assign
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  x *= 3;
+  cout << x;
+  return 0;
+}
+
+
+// /= assign
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  double x = 5;
+  x /= 3;
+  cout << x;
+  return 0;
+}
+
+// %= assign
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  x %= 3;
+  cout << x;
+  return 0;
+}
+
+
+// &= assign
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  x &= 3;
+  cout << x;
+  return 0;
+}
+
+// |= assign
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  x |= 3;
+  cout << x;
+  return 0;
+}
+
+
+// ^= assign
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  x ^= 3;
+  cout << x;
+  return 0;
+}
+
+// >>= assign
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  x >>= 3;
+  cout << x;
+  return 0;
+}
+
+// <<= assign
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  x <<= 3;
+  cout << x;
+  return 0;
+}
