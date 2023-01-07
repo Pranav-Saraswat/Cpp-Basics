497ed2ee5fb06499671a9344d983f14d5d159e7a
Pranav Saraswat <pranavsaraswat20040124@gmail.com>
2023-01-08 02:00:44 +0530

    add one more file

---
+++ cpp/Maths.cpp
@@ -0,0 +1,37 @@
+// Math
+
+// Max
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << max(5, 10);
+  return 0;
+}
+
+// Min
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << min(5, 10);
+  return 0;
+}
+
+
+// <cmath> Header
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  cout << sqrt(64) << "\n";
+  cout << round(2.6) << "\n";
+  cout << log(2) << "\n";
+  return 0;
+}
+
+
