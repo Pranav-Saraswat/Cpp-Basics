94d44424a59ba45989df35c8bbb4e0eff17bbf27
Pranav Saraswat <pranavsaraswat20040124@gmail.com>
2023-01-08 00:33:17 +0530

    change folder

--- string types.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main() {
-  string greeting = "Hello C++";
-  cout << greeting;
-  return 0;
-}
--- varibles.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-  int myNum = 15;   // Now myNum is 15
-  myNum = 10;       // Now myNum is 10
-  cout << myNum;
-  return 0;
-}
